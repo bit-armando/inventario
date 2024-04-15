dc5d2d35833564ff18e250557715d72a8e10b51b
Armando Rosales <refill.1509@gmail.com>
2024-04-15 10:18:36 -0600

    home/views registrar salida/entrada y registro de usuarios

--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Tipo_empleado(models.Model):
+    #TODO Dejar en vista admin
     id_empleado = models.AutoField(primary_key=True)
     tipo = models.CharField(max_length=50)
     
@@ -16,6 +17,7 @@ class Usuario(models.Model):
     telefono = models.CharField(max_length=10)
     direccion = models.CharField(max_length=255)
     contrasena = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
     tipo_empleado = models.ForeignKey(Tipo_empleado, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -23,7 +25,7 @@ class Usuario(models.Model):
     
     
 class Proveedor(models.Model):
-    # id_proveedor = models.CharField(primary_key=True, max_length=10)
+    # TODO Dejar en vista admin
     id_proveedor = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50)
     telefono = models.CharField(max_length=10)
@@ -34,7 +36,7 @@ class Proveedor(models.Model):
     
     
 class Categoria(models.Model):
-    # id_categoria = models.CharField(primary_key=True, max_length=10)
+    # TODO Dejar en vista admin
     id_categoria = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     
@@ -43,13 +45,14 @@ class Categoria(models.Model):
     
 
 class Producto(models.Model):
-    # id_producto = models.CharField(primary_key=True, max_length=10)
+    #TODO Dejar en vista admin
     id_producto = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50, default='')
     descripcion = models.CharField(max_length=255)
     precio_unitario = models.DecimalField(max_digits=10, decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE, default='')
+    #TODO agregar imagen
 
     def __str__(self):
         return self.nombre
--- home/views.py
+++ home/views.py
@@ -1,5 +1,61 @@
 from django.http import HttpResponse
 
+from .models import *
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def add_user(request):
+    if request == 'POST':
+        nombre = request.POST['nombre']
+        apellido = request.POST['apellido']
+        telefono = request.POST['telefono']
+        direccion = request.POST['direccion']
+        contrasena = request.POST['contrasena']
+        Tipo_empleado = request.POST['tipo_empleado']
+        
+        Usuario(nombre=nombre, apellido=apellido, 
+                telefono=telefono, direccion=direccion,
+                contrasena=contrasena,
+                tipo_empleado=Tipo_empleado).save()
+    
+    # TODO crear un formulario para agregar un usuario
+    # TODO crear return donde redireccione a la pagina principal
+    # TODO hashear la contraseña
+    
+
+def registrar_entrada(request):
+    if request == 'POST':
+        provedor = request.POST['proveedor']
+        producto = request.POST['producto']
+        cantidad = request.POST['cantidad']
+        cantidad = request.POST['cantidad']
+        #TODO guardar solo fecha
+        fecha = request.POST['fecha']
+        descripcion = request.POST['descripcion']
+        #TODO guardar a la persona que hizo la entrada
+        
+        Entrada(proveedor=provedor, producto=producto, 
+                cantidad=cantidad, fecha=fecha, descripcion=descripcion).save()
+    
+    # TODO crear un formulario para registrar una entrada
+    # TODO crear return donde redireccione a la pagina principal
+    
+def resgistar_salida(request):
+    if request == 'POST':
+        producto = request.POST['producto']
+        cantidad = request.POST['cantidad']
+        #TODO guardar solo fecha
+        fecha = request.POST['fecha']
+        descripcion = request.POST['descripcion']
+        #TODO guardar a la persona que hizo la salida
+        
+        Salida(producto=producto, cantidad=cantidad, 
+               fecha=fecha, descripcion=descripcion).save()
+        
+    # TODO crear un formulario para registrar una salida
+    # TODO crear return donde redireccione a la pagina principal
+    
+#TODO crear una vista para mostrar los productos en inventario
