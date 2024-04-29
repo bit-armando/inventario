ded973f626c3de559e23f31265369eeead102f4e
Santiago <santiagomendivil1009@gmail.com>
2024-04-29 11:07:27 -0600

    Agregue las conexiones, imagenes y css a VentanaPrincipal.html y Ventas.html

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add-product/', views.add_product, name='add_product'),
+    path('agregar-producto/', views.add_product, name='agregar_producto'),
     path('registrar-entrada/', views.registrar_entrada, name='registrar_entrada'),
     path('principal/', views.ventana_principal, name='ventana_principal'),
     path('ventas/', views.ventas, name="ventas"),
