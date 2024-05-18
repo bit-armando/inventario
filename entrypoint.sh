b096effa89383e04622866ab10ed165c18439915
Armando Rosales <refill.1509@gmail.com>
2024-05-18 14:04:56 -0600

    Cambio de variable por error de acceso

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo 'Running collecstatic...'
-python manage.py collectstatic --no-input --settings=config.settings.production
+python manage.py collectstatic --no-input --settings=config.settings.development
 
 echo 'Applying migrations...'
-python manage.py wait_for_db --settings=config.settings.production
-python manage.py migrate --settings=config.settings.production
+python manage.py wait_for_db --settings=config.settings.development
+python manage.py migrate --settings=config.settings.development
 
 echo 'Running server...'
-gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application --bind 0.0.0.0:8000
+gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.development config.wsgi:application --bind 0.0.0.0:8000
